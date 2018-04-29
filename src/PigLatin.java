7cdfb0ebb8be674c279fc55a743880dd8e71900e
jillxx <jiaoxu84@gmail.com>
2018-04-29 00:20:44 -0400

    using a line

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -7,46 +7,50 @@ public class PigLatin {
 
 	public static void main(String[] args) {
 		String cont = "y";
-		
+
 		while (cont.equalsIgnoreCase("y")) {
-		System.out.println("Welcome to the Pig Latin Translator!");
-		Scanner scnr = new Scanner(System.in);
 
-		// prompt user to input a word
-		System.out.println("Please enter a word!");
-		String input = scnr.nextLine();
+			System.out.println("Welcome to the Pig Latin Translator!");
+			Scanner scnr = new Scanner(System.in);
 
-		// convert to lowercase
-		//input = input.toLowerCase();
+			// prompt user to input a word
+			System.out.println("Please enter a line!");
+			String input = scnr.nextLine();
+			// split the line by whitespace.
+			String[] words = input.split("\\s");
+			for (String w : words) {
+				// convert to lowercase
+				// input = input.toLowerCase();
 
-		// translate to pig latin
-		String output = "";
-		if (isVowel(input) == true) {
-			// if word starts with a vowel (a e i o u), add "way"
-			output = translateVowel(input);
-		} else {
-			// if start with a consonant, move consonant before vowel to the end and add"ay"
-			output = translateConsonant(input);
+				// translate to pig latin
+				String output = "";
+				if (isVowel(w) == true) {
+					// if word starts with a vowel (a e i o u), add "way"
+					output = translateVowel(w);
+				} else {
+					// if start with a consonant, move consonant before vowel to the end and add"ay"
+					output = translateConsonant(w);
+				}
+
+				// display it on the console
+
+				System.out.println(output);
+			}
+			// prompt user for another word
+			// ask the user if he wants too continue
+			System.out.println("\nContinue?(y/n): ");
+			cont = scnr.next();
+
 		}
+		System.out.println("Goodbye!");
 
-		// display it on the console
-
-		System.out.println(output);
-		// prompt user for another word
-		// ask the user if he wants too continue
-		System.out.println("\nContinue?(y/n): ");
-		cont = scnr.next();
-    
-	}
-	System.out.println("Goodbye!");
-	
 	}
 
 	public static boolean isVowel(String word) {
 
 		if (word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o")
-				|| word.startsWith("u") || word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o")
-				|| word.startsWith("u")) {
+				|| word.startsWith("u") || word.startsWith("a") || word.startsWith("e") || word.startsWith("i")
+				|| word.startsWith("o") || word.startsWith("u")) {
 			return true;
 		}
 		return false;
@@ -71,14 +75,13 @@ public class PigLatin {
 			char c = word.charAt(i);
 			int vowelIndex = vowel.indexOf(c);
 			int wordIndex = word.indexOf(c);
-		
 
 			if (vowelIndex == -1) {// if the word does not has a vowel
 				continue;
 			} else {
 				// split the word at the vowel and cast the front part to the end
 				wordAfter = word.substring(wordIndex);
-				
+
 				wordBefore = word.substring(0, wordIndex);
 				break;// if find a vowel, piglatin it and break the loop
 			}
